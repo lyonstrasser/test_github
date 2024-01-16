f966bb0b1899e93c6ab34f143016b94d57f594b4
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-16 17:50:52 +0100

    Created new class test

---
+++ src/geometry/Test.java
@@ -0,0 +1,14 @@
+package geometry;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Rectangle r = new Rectangle(3, 4, 5, 2);
+		System.out.println(r);
+		System.out.println(r.circumference());
+		System.out.println(r.area());
+		
+	}
+
+}
