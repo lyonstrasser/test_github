41d7f04a329defdc1bcbde052b6c300def31f4a8
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-16 09:44:14 +0100

    Methods for calculating area and circumference in Circle class

--- src/geometry/Circle.java
+++ src/geometry/Circle.java
@@ -1,17 +1,25 @@
 package geometry;
 
 public class Circle extends Body {
-
+	
+	private double radius;
+	public final double pi = 3.14;
+	
+	public Circle(double x, double y, double radius) {
+		this.setxCoordinate(x);
+		this.setyCoordinate(y);
+		this.radius = radius;
+	}
+	
+	
 	@Override
 	public double circumference() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 2 * this.pi * this.radius;
 	}
 
 	@Override
 	public double area() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.pi * this.radius * this.radius;
 	}
 
 }
