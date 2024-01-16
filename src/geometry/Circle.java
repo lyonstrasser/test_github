8f17a249ce0ca606e5d928da73aa2759e67a909b
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-16 09:27:46 +0100

    Created new class Circle

---
+++ src/geometry/Circle.java
@@ -0,0 +1,17 @@
+package geometry;
+
+public class Circle extends Body {
+
+	@Override
+	public double circumference() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
