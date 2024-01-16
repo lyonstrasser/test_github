94b7b0f026c63a80ff72ac632ee4b6a31a91cbc4
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-16 09:43:58 +0100

    Methods for calculating area and circumference in Triangle class

--- src/geometry/Triangle.java
+++ src/geometry/Triangle.java
@@ -1,5 +1,29 @@
 package geometry;
 
-public class Triangle {
+public class Triangle extends Body {
+	
+	private double siteA;
+	private double siteB;
+	private double siteC;
+	private double height;
+	
+	public Triangle(double x, double y, double a, double b, double c, double h) {
+		this.setxCoordinate(x);
+		this.setyCoordinate(y);
+		this.siteA = a;
+		this.siteB = b;
+		this.siteC = c;
+		this.height = h;
+	}
+	
+	@Override
+	public double circumference() {
+		return (this.siteA + this.siteB + this.siteC);
+	}
+
+	@Override
+	public double area() {
+		return ((this.siteA / 2) * this.height);
+	}
 
 }
