c800ebe7a32b437e7d78c0ec013929aedc5662b9
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-22 11:01:05 +0100

    Added all the methods for the Group class

--- src/geometry/Group.java
+++ src/geometry/Group.java
@@ -1,5 +1,59 @@
 package geometry;
 
-public class Group {
+import java.util.ArrayList;
 
+public class Group {
+	
+	ArrayList<Body> bodies = new ArrayList<Body>();
+	
+	/**
+	 * Adds a body to the list of all bodies
+	 * @param body
+	 */
+	public void addBody(Body body) {
+		this.bodies.add(body);
+	}
+	
+	/**
+	 * Removes a body from the list of all bodies
+	 * @param body
+	 */
+	public void removeBody(Body body) {
+		this.bodies.remove(body);
+	}
+	
+	/**
+	 * Calculates the whole circumference of all bodies 
+	 * @return
+	 */
+	public double overallCircumference() {
+		double circumference = 0;
+		for(Body b : this.bodies) {
+			circumference += b.circumference();
+		}
+		return circumference;
+	}
+	
+	/**
+	 * Calculates the whole area of all bodies
+	 * @return
+	 */
+	public double overallArea() {
+		double area = 0;
+		for(Body b : this.bodies) {
+			area += b.area();
+		}
+		System.out.println(area);
+		return area;
+	}
+	
+	/**
+	 * Prints out all the bodies from the list on the console
+	 */
+	public void printBodies() {
+		for(Body b : this.bodies) {
+			System.out.println(b);
+		}
+	}
+	
 }
