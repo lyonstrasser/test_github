79caa70b1e884e78502405a9e6591595a640c6c4
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-29 11:35:28 +0100

    New class "Submission"

---
+++ src/socialNetwork/Submission.java
@@ -0,0 +1,12 @@
+package socialNetwork;
+
+import java.util.ArrayList;
+
+public abstract class Submission {
+	
+	private String username;
+	private long timestamp;
+	private int likes;
+	private ArrayList<Comment> comment;
+	
+}
