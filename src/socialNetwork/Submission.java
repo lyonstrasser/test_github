0f64e88aff48bae1c09f1b9b6f43e33b47510bd1
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-29 21:27:10 +0100

    Created a ArrayList for the Submissions

--- src/socialNetwork/Submission.java
+++ src/socialNetwork/Submission.java
@@ -7,6 +7,36 @@ public abstract class Submission {
 	private String username;
 	private long timestamp;
 	private int likes;
-	private ArrayList<Comment> comment;
+	private ArrayList<Comment> comments;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	
+	
+	public void giveLike() {
+		likes = this.likes + 1;
+	}
+	
+	public void giveComment(Comment c) {
+		this.comments.add(c);
+	}
+	
 	
 }
