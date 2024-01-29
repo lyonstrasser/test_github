4a5d214f9fbf5126f25d46ed052287cc92843298
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-29 21:22:16 +0100

    Added the attributes for the the classes

--- src/socialNetwork/Message.java
+++ src/socialNetwork/Message.java
@@ -1,5 +1,28 @@
 package socialNetwork;
 
-public class Message {
+public class Message extends Submission {
+	
+	private String message;
 
+	public Message(String user, String message) {
+		this.setUsername(user);
+		this.message = message;
+	}
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String toString() {
+		return "User: " + this.getUsername() + "\nMessage: " + this.message +
+				"\nLikes: " + this.getLikes() + "\nComments: ";			// Zeitstempel? Liste der Kommentare?
+	}
+
+	
+	
 }
--- src/socialNetwork/Photo.java
+++ src/socialNetwork/Photo.java
@@ -1,5 +1,29 @@
 package socialNetwork;
 
-public class Photo {
-
+public class Photo extends Submission{
+	
+	private String filename;
+	private String headline;
+	
+	public Photo(String user, String filename, String headline) {
+		this.setUsername(user);
+		this.filename = filename;
+		this.headline = headline;
+	}
+	
+	public String getFileName() {
+		return filename;
+	}
+	public void setFileName(String fileName) {
+		this.filename = fileName;
+	}
+	public String getHeadline() {
+		return headline;
+	}
+	public void setHeadline(String headline) {
+		this.headline = headline;
+	}
+	
+	
+	
 }
