ec36f65df9a7441fb7d134ab610ebd08c4238372
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-30 09:08:28 +0100

    Updated the Constructor and added new Method "printAllComments"

--- src/socialNetwork/Submission.java
+++ src/socialNetwork/Submission.java
@@ -1,14 +1,22 @@
 package socialNetwork;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public abstract class Submission {
 	
 	private String username;
-	private long timestamp;
+	private LocalDate timestamp;
 	private int likes;
 	private ArrayList<Comment> comments;
 	
+	public Submission(String username) {
+		super();
+		this.username = username;
+		this.timestamp = LocalDate.now();
+		this.likes = 0;
+		this.comments = new ArrayList<Comment>();
+	}
 	
 	public String getUsername() {
 		return username;
@@ -16,10 +24,10 @@ public abstract class Submission {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	public long getTimestamp() {
+	public LocalDate getTimestamp() {
 		return timestamp;
 	}
-	public void setTimestamp(long timestamp) {
+	public void setTimestamp(LocalDate timestamp) {
 		this.timestamp = timestamp;
 	}
 	public int getLikes() {
@@ -30,12 +38,18 @@ public abstract class Submission {
 	}
 	
 	
-	public void giveLike() {
+	public void addLike() {
 		likes = this.likes + 1;
 	}
 	
-	public void giveComment(Comment c) {
+	public void addComment(Comment c) {
 		this.comments.add(c);
+	}
+	
+	public void printAllComments() {
+		for(Comment c : this.comments) {
+			System.out.println(c);
+		}
 	}
 	
 	
