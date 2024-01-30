3e7978cd919a7a8fe85a10c547cd5800a2bbb0f4
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-01-30 09:07:39 +0100

    Added toString-method

--- src/socialNetwork/Comment.java
+++ src/socialNetwork/Comment.java
@@ -2,7 +2,30 @@ package socialNetwork;
 
 public class Comment {
 	
+	private String user;
+	private String comment;
 	
+	public Comment (String user, String comment) {
+		this.user = user;
+		this.comment = comment;
+	}
 	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 	
+	public String toString() {
+		return "Comment:\n\t " + this.comment + "\nfrom:\n\t" + this.user;
+	}
+	
+
 }
