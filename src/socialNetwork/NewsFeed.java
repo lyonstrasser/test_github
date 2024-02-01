fa0bad0efeacc66cb33d80c421d1a8cca8d38916
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-02-01 10:44:36 +0100

    Adding the required methods for the class NewsFeed

--- src/socialNetwork/NewsFeed.java
+++ src/socialNetwork/NewsFeed.java
@@ -1,5 +1,40 @@
 package socialNetwork;
 
-public class NewsFeed {
+import java.util.ArrayList;
 
+public class NewsFeed {
+	
+	ArrayList<Submission> newsfeed = new ArrayList<Submission>();
+	
+	public void addSubmission(Submission s) {
+		this.newsfeed.add(s);
+	}
+	
+	public void removeSubmission(Submission s) {
+		this.newsfeed.remove(s);
+	}
+	
+	public void getSubmissionsFromUser(String author){
+		for(Submission s : this.newsfeed) {
+			if(s.getUsername().equals(author)) {
+				System.out.println(s);
+			}
+		}
+	}
+	
+	public void printAllSubmissions() {
+		for(Submission s : this.newsfeed) {
+			System.out.println(s);
+		}
+	}
+	
+	public void removeSubmission(String author) {
+		for(Submission s : this.newsfeed) {
+			if(s.getUsername().equals(author)) {
+				this.newsfeed.remove(s);
+			}
+		}
+	}
+	
+	
 }
