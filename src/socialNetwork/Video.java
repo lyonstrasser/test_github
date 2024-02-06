c57f46902cdf1bfff59c13e30405fc5891d0043c
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-02-06 09:19:30 +0100

    Adding new class "Video"

---
+++ src/socialNetwork/Video.java
@@ -0,0 +1,15 @@
+package socialNetwork;
+
+public class Video extends Photo {
+
+	public Video(String username, String filename, String headline) {
+		super(username, filename, headline);
+	}
+	
+	public String toString() {
+		return "Video:\n\tUser: " + this.getUsername() + "\n\tHeadline: " + this.getHeadline() +
+				"\n\tFilename: " + this.getFileName() + "\n\tLikes: " + this.getLikes() + 
+				"\n\tTimeSpan: " + this.calculateTimeSpan();
+	}
+	
+}
