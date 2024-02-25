344b90d2ac7b8320a3c8d3671fcbfb9bfba8fb37
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-02-25 20:32:59 +0100

    Finishing exercise 2

---
+++ src/array_exercises/exercise_2.java
@@ -0,0 +1,25 @@
+package array_exercises;
+
+public class exercise_2 {
+	
+	
+	public static boolean isProbablyApproaching(String[] signs) {
+		for(int i = 0; i < signs.length; i++) {
+			if(signs.length == 0 || signs[i].equals(null)) {
+				System.out.println("Fehler!");
+				return false;
+			}
+			else if(signs[i].equals(signs[i-1]) && signs[i].equals(signs[i-2]) && signs[i].equals(signs[i-3])){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
