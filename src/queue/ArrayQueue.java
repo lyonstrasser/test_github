d1f3eb67d84ec6158af91f85a5291c221d6354d9
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-04-07 22:05:58 +0200

    Testing the methods

---
+++ src/queue/ArrayQueue.java
@@ -0,0 +1,68 @@
+package queue;
+
+public class ArrayQueue {
+	
+	private static int front;
+	private static int rear;
+	private static int capacity;
+	private static int[] queue;
+	
+	public ArrayQueue(int size) {
+		front = 0;
+		rear = 0;
+		capacity = size;
+		queue = new int[size];
+	}
+	
+	public  void queueEnqueue(int item) {
+		if(rear == capacity) {
+			System.out.println("Queue ist voll");
+		} else {
+			queue[rear] = item;
+			rear ++;
+		}
+	}
+	
+	public  int queueDequeue() {
+		int firstElement = 0;
+		if(rear == front) {
+			System.out.println("Queue ist leer!");
+		} else {
+			firstElement = queue[front];
+			for(int i = 0; i < (rear-1); i++) {
+				queue[i] = queue[i+1];
+			}
+			if(rear < capacity) {
+				queue[rear] = 0;
+			}
+			rear --;
+		}
+		return firstElement;
+	}
+	
+	public void queueDisplay() {
+		if(front == rear) {
+			System.out.println("Queue is empty!");
+		} else {
+			for(int i = front; i < rear; i++) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	public int queueFront() {
+		// TODO erstes Element ohne Löschen zurückgeben
+		int firstElement = 0;
+		if(front == rear) {
+			System.out.println("Queue is empty!");
+		} else {
+			firstElement = queue[front];
+		}
+		return firstElement;
+	}
+	
+	// HÜ: ArrayQueueTest - alle Methoden von ArrayQueue testen
+	// JavaDoc-Kommentare
+	// Kapitel 5 nochmals durcharbeiten
+	
+}
---
+++ src/queue/test.java
@@ -0,0 +1,16 @@
+package queue;
+
+public class test {
+	
+	ArrayQueue queue = new ArrayQueue(10);
+	
+	queue.queueEnqueue(3);
+	
+	queue.queueDequeue();
+	
+	queue.queueDisplay();
+	
+	queue.queueFront();
+	
+	
+}
