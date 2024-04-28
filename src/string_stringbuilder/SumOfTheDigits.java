ba455acd380662e5875f140d750c890782fee5ac
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-04-28 20:59:49 +0200

    Zusätzlliche Methode

--- src/string_stringbuilder/SumOfTheDigits.java
+++ src/string_stringbuilder/SumOfTheDigits.java
@@ -6,6 +6,7 @@ public class SumOfTheDigits {
 	 
 		public static int digitSum(String value) {
 			LinkedList<Integer> stack = new LinkedList<>();
+			
 			for (String s : value.split("")) {
 				stack.push(Integer.parseInt(s));
 			}
@@ -15,7 +16,28 @@ public class SumOfTheDigits {
 				int result = num1 + num2;
 				stack.push(result);
 			}
+			
 			return stack.pop();
+			
+		}
+		
+		
+		public static int digitSum(long value) {
+			String str = Long.toString(value);
+			LinkedList<Integer> stack = new LinkedList<>();
+			
+			for (String s : str.split("")) {
+				stack.push(Integer.parseInt(s));
+			}
+			while (!(stack.size() == 1)) {
+				int num1 = stack.pop();
+				int num2 = stack.pop();
+				int result = num1 + num2;
+				stack.push(result);
+			}
+			
+			return stack.pop();
+			
 		}
 		
 	
@@ -23,7 +45,8 @@ public class SumOfTheDigits {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(digitSum("10"));
+		System.out.println(digitSum(25));
+		System.out.println(digitSum("25"));
  
 		
 	}
