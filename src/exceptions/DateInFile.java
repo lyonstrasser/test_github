9c77415e4ae68e10007b26c565f8751160e0aa07
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-05-27 09:15:19 +0200

    Homework to GIT

--- src/exceptions/DateInFile.java
+++ src/exceptions/DateInFile.java
@@ -1,11 +1,20 @@
 package exceptions;
 
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 public class DateInFile {
 
 	public static void main(String[] args) {
 
+		String file = "test.txt";
 		
-		
+		try(PrintWriter writer = new PrintWriter(file)){
+			writer.write(LocalDateTime.now().toString());
+			System.out.println("Erfolgreich überschrieben!");
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
 		
 	}
 
