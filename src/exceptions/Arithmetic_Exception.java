8ab8ae23dd1fff00e39c163abd8542b0ebfb7321
lyonstrasser <Lyon.Strasser@hak-reutte.ac.at>
2024-05-28 13:00:39 +0200

    implementation finished

--- src/exceptions/Arithmetic_Exception.java
+++ src/exceptions/Arithmetic_Exception.java
@@ -10,6 +10,25 @@ public class Arithmetic_Exception {
 		}
 	}
 	
+	public static int checkFileExtension(String file) {
+		
+		if(file.endsWith(".java")){
+			System.out.println("1");
+			return 1;
+		} 
+		try {
+			if (file.isEmpty()) {
+				throw new FileIsEmptyException();
+			}
+				
+		} catch (FileIsEmptyException e) {
+			System.out.println("File ist leer!");
+			return -1;
+		}
+		return 0;
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		try {
@@ -17,6 +36,8 @@ public class Arithmetic_Exception {
 		} catch (ArithmeticException e) {
 			System.out.println("No division with 0");
 		}
+		
+		checkFileExtension("text.java");
 		
 	}
 
